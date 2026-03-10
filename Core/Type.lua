f4ec279c5d1ae015a22e98519e695c4dabfe219c
Zhou Fang <fang2hou@outlook.com>
2026-03-10 15:15:03 +0900

    feat: update custom type

--- Core/Type.lua
+++ Core/Type.lua
@@ -1,1 +1,9 @@
----@alias LuaValueReference any
+---[Documentation](https://warcraft.wiki.gg/wiki/API_GameTooltip_SetText)
+---@param text string
+---@param colorR? number
+---@param colorG? number
+---@param colorB? number
+---@param alpha? number
+---@param wrap? boolean
+---@diagnostic disable-next-line: duplicate-set-field
+function GameTooltip:SetText(text, colorR, colorG, colorB, alpha, wrap) end
